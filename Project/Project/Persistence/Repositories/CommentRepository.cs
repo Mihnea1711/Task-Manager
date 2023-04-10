4999f37a4c31d9b41c2410685fe03aeae7108c15
Mihnea1711 <100217542+Mihnea1711@users.noreply.github.com>
2023-04-10 18:14:29 +0300

    test

--- Project/Project/Persistence/Repositories/CommentRepository.cs
+++ Project/Project/Persistence/Repositories/CommentRepository.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Project.Models.Interfaces
+namespace Project.Persistence.Interfaces
 {
-    internal interface ISubtaskModel
+    internal class CommentRepository
     {
     }
 }
