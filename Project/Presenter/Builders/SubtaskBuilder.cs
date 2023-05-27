15e13920ef155422d1f15a65376b989d0ce94842
Mihnea1711 <100217542+Mihnea1711@users.noreply.github.com>
2023-05-27 13:20:53 +0300

    add header text to all files

--- Project/Presenter/Builders/SubtaskBuilder.cs
+++ Project/Presenter/Builders/SubtaskBuilder.cs
@@ -1,4 +1,18 @@
-﻿using System;
+﻿/**************************************************************************
+ *                                                                        *
+ *  Description: Task Management Tool                    		          *
+ *  Website Mihnea: https://github.com/Mihnea1711               	      *
+ *  Website Robert: https://github.com/cioocan               	          *
+ *  Copyright:   (c) 2023, Mihnea Bejinaru, Robert Ciocan                 *
+ *                                                                        *
+ *  This code and information is provided "as is" without warranty of     *
+ *  any kind, either expressed or implied, including but not limited      *
+ *  to the implied warranties of merchantability or fitness for a         *
+ *  particular purpose. You are free to use this source code in your      *
+ *  applications as long as the original copyright notice is included.    *
+ *                                                                        *
+ **************************************************************************/
+
 using System.Windows.Forms;
 
 namespace Presenters
